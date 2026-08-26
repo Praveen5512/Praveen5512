d2365a01ff06c7977ee5631f993b5587f22a28f3
Praveen5512 <praveen2015slv@gmail.com>
2026-08-26 08:08:31 +0530

    Under Development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+
+import { colors } from './constant';
+import HeroSection from './components/HeroSection';
+import AboutMe from './components/AboutMe';
 
 function App() {
+  const [active, setActive] = useState("home");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container" style={{backgroundColor:colors.primary['black-100'], color:colors.secondary['white-100']}}>
+      <Navbar active={active} setActive={setActive}/>
+      <HeroSection/>
+      {/* <AboutMe/> */}
     </div>
   );
 }
